68286237fed6145e1af4b06163cbe67af2f364e6
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-12-25 22:23:26 +0100

    Remove self modified version

--- 01_splitlab/splitlab/Tools/epochTimeToIrisString.m
+++
@@ -1,6 +0,0 @@
-function [TimeString] = epochTimeToIrisString(epochTime)
-% Convert from an epoch time to a irisFetch style time string
-% Rob Porritt, July 2014
-    TimeString = datestr(epochTime,'yyyy-mm-dd HH:MM:SS.FFF');
-end
-
