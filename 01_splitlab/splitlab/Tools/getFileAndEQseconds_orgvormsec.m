65c614bcf3f4d50b6e1925d000b479f9e13358d1
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-12-26 21:32:10 +0100

    Add fix for SplitLab SLmsec bug

--- 01_splitlab/splitlab/Tools/getFileAndEQseconds_orgvormsec.m
+++ 01_splitlab/splitlab/Tools/getFileAndEQseconds_orgvormsec.m
@@ -11,17 +11,6 @@ function [FIsec, FIyyyy, EQsec, Omarker] = getFileAndEQseconds(F,eqin,offset)
 % Windows user can try a renamer , for example 1-4aren (one-for all renamer)
 % http://www.1-4a.com/rename/ perhaps this adress is still valid
 
-
-%==========================================================================
-% Yvonne Fröhlich (YF), Karlsruhe Institute of Technology (KIT), 
-% Email: yvonne.froelich@kit.edu
-% 2021
-%
-% modification to fix bug in SplitLab of unconsidered msec of start time (SLmsec)
-%
-%==========================================================================
-
-
 global config
 
 
@@ -33,37 +22,23 @@ if config.UseHeaderTimes | strcmp(config.FileNameConvention, '*.e; *.n; *.z')
         catch
             sac = sl_rsacsun([config.datadir filesep F(k,:)]);
         end
-        [FIyyyy(k), FIddd(k), FIHH(k), FIMM(k), FISS(k), FImmm(k)] =...
-            sl_lh(sac, 'NZYEAR','NZJDAY','NZHOUR','NZMIN', 'NZSEC', 'NZMSEC'); % YF add msec 2021/06/24
+        [FIyyyy(k), FIddd(k), FIHH(k), FIMM(k), FISS(k)] =...
+            sl_lh(sac, 'NZYEAR','NZJDAY','NZHOUR','NZMIN', 'NZSEC'); 
         Omarker(k) = sl_lh(sac, 'O');
     end
     
      Omarker(Omarker == -12345) = 0;  %verify, if O-marker is set   
-     %FIsec  =  FISS + FIMM*60 + FIHH*3600 + (FIddd)*86400 + Omarker;
-     FIsec  =  FImmm/1000 + FISS + FIMM*60 + FIHH*3600 + (FIddd)*86400 + Omarker; % YF add msec 2021/06/24
+     FIsec  =  FISS + FIMM*60 + FIHH*3600 + (FIddd)*86400 + Omarker;
 
     fclose all;
 
 
 
 else % USE FILENAME
-
-	% YF add warning 2021/Nov/28
-    msgbox( {'When extracting the \bfstart time\rm from the \bffile name\rm be sure that this time is \bf\itreally\rm the exact \bfstart time\rm of the \bftrace\rm!'}, ...
-            'Check start time' ,'warn', ...
-            struct('WindowStyle',{'modal'},'Interpreter',{'tex'}) ); 
-        
     switch config.FileNameConvention
-	
         case 'mseed2sac1'
-            % mseed2sac format: QT.ANIL.00.HHZ.D.2012,321,18:00:00.SAC % YF year,jday,hour:min:sec
-            
-			% YF add waring 2021/Nov/28
-			msgbox( 'Only correct for \bfzero milliseconds\rm!', ...
-                    'Check milliseconds' ,'warn', ...
-                    struct('WindowStyle',{'modal'},'Interpreter',{'tex'}) );
-            
-			FIsec = zeros(1,length(F(:,1)));
+            % mseed2sac format: QT.ANIL.00.HHZ.D.2012,321,18:00:00.SAC
+            FIsec = zeros(1,length(F(:,1)));
             FIyyyy = zeros(1,length(F(:,1)));
             
             % Loop through each file
@@ -96,14 +71,8 @@ else % USE FILENAME
             
         case 'mseed2sac2'
             % seems mseed2sac changed naming convention...
-            % mseed2sac format: IU.HRV.00.BH1.M.2011.246.044859.SAC % YF year.jday.hourminsec
-            
-			% YF add waring 2021/Nov/28
-			msgbox( 'Only correct for \bfzero milliseconds\rm!', ...
-                    'Check milliseconds' ,'warn', ...
-                    struct('WindowStyle',{'modal'},'Interpreter',{'tex'}) );
-            
-			FIsec = zeros(1,length(F(:,1)));
+            % mseed2sac format: IU.HRV.00.BH1.M.2011.246.044859.SAC
+            FIsec = zeros(1,length(F(:,1)));
             FIyyyy = zeros(1,length(F(:,1)));
             
             % Loop through each file
@@ -130,29 +99,24 @@ else % USE FILENAME
                FIMM      = str2double(F(i,dots(7)+3:dots(7)+4));
                FISS      = str2double(F(i,dots(7)+5:dots(end)-1));
                FIsec(i)  = FISS + FIMM * 60 + FIHH * 3600 + (FIddd) * 86400;  
-            end            
+            end
+            
                     
         case 'RDSEED'
-            % RDSEED format '1993.159.23.15.09.7760.IU.KEV..BHN.D.SAC' % YF year.jday.hour.min.sec.msec
+            % RDSEED format '1993.159.23.15.09.7760.IU.KEV..BHN.D.SAC' 
             FIyyyy = str2num(F(:,1:4));
             FIddd  = str2num(F(:,6:8));
             FIHH   = str2num(F(:,10:11));
             FIMM   = str2num(F(:,13:14));
             FISS   = str2num(F(:,16:17));
             FIMMMM = str2num(F(:,18:22));
-            FIsec  = FIMMMM + FISS + FIMM*60 + FIHH*3600 + (FIddd)*86400; 
-			% YF no division by 1000 or 10000 for msec to get sec 
-			% because position 18 is the dot therefore .xxxx, so 0.xxxx
+            FIsec  = FIMMMM + FISS + FIMM*60 + FIHH*3600 + (FIddd)*86400;
+
+
 
         case 'SEISAN'
-            % SEISAN format '2003-05-26-0947-20S.HOR___003_HORN__BHZ__SAC' % YF year-month-day-minsec
-            
-			% YF add waring 2021/Nov/28
-			msgbox( 'Only correct for \bfzero milliseconds\rm!', ...
-                    'Check milliseconds' ,'warn', ...
-                    struct('WindowStyle',{'modal'},'Interpreter',{'tex'}) );
-            
-			FIyyyy = str2num(F(:,1:4));
+            % SEISAN format '2003-05-26-0947-20S.HOR___003_HORN__BHZ__SAC'
+            FIyyyy = str2num(F(:,1:4));
             FImonth= str2num(F(:,6:7));
             FIdd   = str2num(F(:,9:10));
             FIHH   = str2num(F(:,12:13));
@@ -162,15 +126,10 @@ else % USE FILENAME
             FIddd = dayofyear(FIyyyy',FImonth',FIdd')';%julian Day
             FIsec =  FISS + FIMM*60 + FIHH*3600 + (FIddd)*86400;
 
+
         case 'YYYY.JJJ.hh.mm.ss.stn.sac.e'
-            % Format: 1999.136.15.25.00.ATD.sac.z % YF year.jday.hour.min.sec
-            
-			% YF add waring 2021/Nov/28
-			msgbox( 'Only correct for \bfzero milliseconds\rm!', ...
-                    'Check milliseconds' ,'warn', ...
-                    struct('WindowStyle',{'modal'},'Interpreter',{'tex'}) );
-            
-			FIyyyy = str2num(F(:,1:4));
+            %  Format: 1999.136.15.25.00.ATD.sac.z
+            FIyyyy = str2num(F(:,1:4));
             FIddd  = str2num(F(:,6:8));
             FIHH   = str2num(F(:,10:11));
             FIMM   = str2num(F(:,13:14));
@@ -178,14 +137,8 @@ else % USE FILENAME
             FIsec  = FISS + FIMM*60 + FIHH*3600 + (FIddd)*86400;
 
         case 'YYYY.MM.DD-hh.mm.ss.stn.sac.e';
-            % Format: 2003.10.07-05.07.15.DALA.sac.z % YF year.month.day-hour.min.sec
-            
-			% YF add waring 2021/Nov/28
-			msgbox( 'Only correct for \bfzero milliseconds\rm!', ...
-                    'Check milliseconds' ,'warn', ...
-                    struct('WindowStyle',{'modal'},'Interpreter',{'tex'}) );
-            
-			FIyyyy = str2num(F(:,1:4));
+            % Format: 2003.10.07-05.07.15.DALA.sac.z
+            FIyyyy = str2num(F(:,1:4));
             FImonth= str2num(F(:,6:7));
             FIdd   = str2num(F(:,9:10));
             FIHH   = str2num(F(:,12:13));
@@ -196,14 +149,8 @@ else % USE FILENAME
             FIsec  =  FISS + FIMM*60 + FIHH*3600 + (FIddd)*86400;
             
         case 'YYYY_MM_DD_hhmm_stnn.sac.e';
-            % Format: 2005_03_02_1155_pptl.sac (LDG/CEA data) % YF year_month_day_hourmin
-            
-			% YF add waring 2021/Nov/28
-			msgbox( 'Only correct for \bfzero seconds\rm!', ...
-                    'Check seconds' ,'warn', ...
-                    struct('WindowStyle',{'modal'},'Interpreter',{'tex'}) );
-            
-			FIyyyy = str2num(F(:,1:4));
+            % Format: 2005_03_02_1155_pptl.sac (LDG/CEA data)
+            FIyyyy = str2num(F(:,1:4));
             FImonth= str2num(F(:,6:7));
             FIdd   = str2num(F(:,9:10));
             FIHH   = str2num(F(:,12:13));
@@ -213,15 +160,8 @@ else % USE FILENAME
             FIsec = FIMM*60 + FIHH*3600 + (FIddd)*86400;
 
         case 'stn.YYMMDD.hhmmss.e'
-            % Format: fp2.030723.213056.X (BroadBand OBS data) % YF yearmonthday.hourminsec
-            
-			% YF add waring 2021/Nov/28
-			msgbox( {'Only correct for \bfzero milliseconds\rm!'; ...
-					'Only correct for \bfyear 2000 or later\rm!'}, ...
-                    'Check milliseconds and year' ,'warn', ...
-                    struct('WindowStyle',{'modal'},'Interpreter',{'tex'}) );
-            
-			FIyyyy = 2000 + str2num(F(:,5:6));%only two-digit year identifier => add 2000, assuming no OBS data before 2000
+            % Format: fp2.030723.213056.X (BroadBand OBS data)
+            FIyyyy = 2000 + str2num(F(:,5:6));%only two-digit year identifier => add 2000, assuming no OBS data before 2000
             FImonth= str2num(F(:,7:8));
             FIdd   = str2num(F(:,9:10));
             FIHH   = str2num(F(:,12:13));
